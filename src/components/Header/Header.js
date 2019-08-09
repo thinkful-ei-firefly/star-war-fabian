02b419221f9f93c8efbb954aef73b478613544cf
Fabian Lema <fabianlemac@gmail.com>
2019-08-09 17:04:39 -0500

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render(){
-    return <div><h1>Character Searcher</h1></div>
+    return <div><h1>Searcher</h1></div>
   }
 }
 
